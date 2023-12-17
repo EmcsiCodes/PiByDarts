7c4f3ed92efa055ed73f28eeec8eb49962231869
EmcsiCodes <kasztaemmanuel@gmail.com>
2023-12-17 23:36:44 +0200

    k

--- index.js
+++ index.js
@@ -2,7 +2,16 @@ const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
 function main(){
-    
+    const dartNum = 10000000;
+    const Ts = dartNum;
+    let Tc = 0;
+    for(let i = 0; i< dartNum; i++){
+        let x = Math.random() * (1 + 1) - 1;
+        let y = Math.random() * (1 + 1) - 1;
+        if( x*x + y*y <= 1) Tc++;
+    }
+    let pi = 4 * (Tc/Ts);
+    console.log(pi);
 }
 
 main();
